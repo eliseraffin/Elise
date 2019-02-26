54699a2c53f5d22102909a92afd1581c1e2a1660
Elise Raffin <eraffin@pcalb-mm0000.univ-jfc.fr>
2019-02-26 11:07:03 +0100

    première version du projet

---
+++ src/tuto_git2/Contrôleur.java
@@ -0,0 +1,21 @@
+package tuto_git2;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Contrôleur implements ActionListener {
+
+	Modèle refModl;
+	
+	public Contrôleur(Modèle m) {
+		this.refModl = m;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		refModl.incrémente();
+	}
+
+	
+}
